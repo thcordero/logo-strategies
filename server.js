fc55e3db9410947da65c7eef4a7e3d96471754e0
Tomas Hidalgo <tomashcordero@gmail.com>
2021-06-22 21:45:15 +1200

    Make it better

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+//-----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------------------------------
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server started...");
+});
